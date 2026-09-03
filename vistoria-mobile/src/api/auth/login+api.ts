9ed474f00ef54a2c555541753c741d4cf4a4abd7
JoaoGW <cpsenha@gmail.com>
2026-09-03 11:51:25 -0300

    (feat/docs) [Mobile]: Adicionada API Request para o Login. Adicionada documentação para Requisições de API, contendo regras e padrões de implementação.

---
+++ vistoria-mobile/src/api/auth/login+api.ts
@@ -0,0 +1,60 @@
+// Autenticar um usuário já cadastrado
+export async function POST(request: Request) {
+  try {
+    if (request.method !== "POST") {
+      return Response.json(
+        {
+          error:
+            "O método " +
+            request.method +
+            " na requisição não é um método válido",
+        },
+        { status: 405 },
+      );
+    }
+
+    const { email, senha } = await request.json();
+
+    if (!email || !senha) {
+      return Response.json(
+        {
+          error:
+            "Não foram disponibilizados e-mail e senha no envio da requisição",
+        },
+        { status: 400 },
+      );
+    }
+
+    const headers = new Headers({
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    });
+
+    const apiUrl = process.env.APIS_URL + "/usuarios/login";
+
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers,
+      body: JSON.stringify({ email, password: senha }),
+    });
+
+    if (!response.ok) {
+      return Response.json(
+        {
+          error: "Erro no processo de response da API - Login de Usuário",
+        },
+        { status: response.status },
+      );
+    }
+
+    const data = await response.json();
+    return Response.json(data);
+  } catch (error) {
+    return Response.json(
+      {
+        error: "Erro encontrado na API - Login de Usuário: " + error,
+      },
+      { status: 500 },
+    );
+  }
+}
